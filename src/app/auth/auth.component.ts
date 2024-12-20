e8c0b49fd55978f6a6b73d56475284b42e04f092
MESSI3214575 <huamanparinalucasedu@gmail.com>
2024-12-20 15:53:54 -0600

    Mensaje

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent {
+  authForm: FormGroup;
+  error = '';
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.authForm = this.fb.group({
+      codigo: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    const { codigo, password } = this.authForm.value;
+    if (codigo === 'sm76058274' && password === '24092005') {
+      sessionStorage.setItem('codigo', codigo);
+      this.router.navigate(['/main']);
+    } else {
+      this.error = 'Código o contraseña incorrectos';
+    }
+  }
 }
